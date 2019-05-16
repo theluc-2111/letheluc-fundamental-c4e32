dc86ab2e9d9ab31356083f21d46b89db65a4c668
theluc-2111 <letheluc.aof@gmail.com>
2019-05-17 00:55:13 +0700

    initial commit

--- Session 4/homework/ex4.py
+++ Session 4/homework/ex4.py
@@ -6,7 +6,7 @@ so_cau_dung = 0
 for i in range(len(goi_cau_hoi)):
     bo_cau_hoi = goi_cau_hoi[i]
     phuong_an = bo_cau_hoi['choice']
-    print(bo_cau_hoi['question'])
+    print('Question',i+1,":",bo_cau_hoi['question'])
     for k,l in phuong_an.items():
         print(k,'.',l)
     your_choice = int(input('Your choice: '))
@@ -14,5 +14,6 @@ for i in range(len(goi_cau_hoi)):
         print("Bingo. That's great!")
         so_cau_dung += 1
     else:
-        print("You're wrong.The answer of this question is",phuong_an[bo_cau_hoi['answer']],',so the right choice must be:',bo_cau_hoi['answer'])
-print('You correctly answer',so_cau_dung,'out of',len(goi_cau_hoi),'questions')
+        print("You're wrong.The answer of this question is",phuong_an[bo_cau_hoi['answer']],
+        '=> so the right choice must be:',bo_cau_hoi['answer'])
+print("It's finished. You correctly answer",so_cau_dung,'out of',len(goi_cau_hoi),'questions')
